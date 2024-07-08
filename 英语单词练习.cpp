2d73a52e0bc7a401db2160b131f157780874b119
whale086 <2312702310@qq.com>
2024-07-08 14:06:42 +0800

    Add files via upload

---
+++ 英语单词练习.cpp
@@ -0,0 +1,255 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+using namespace std;
+//#define int long long
+struct Word{
+	Word(){}
+	string wd="";
+	string mean="";
+	bool operator==(const Word &o){
+		return this->mean==o.mean&&this->wd==o.wd;
+	}
+	bool operator<(const Word &o){
+		return this->mean<o.mean||this->wd<o.wd;
+	}
+	
+};
+vector<Word>reference;
+/*
+大英3
+1 pursuit 对··的追求
+2 inhibit 抑制
+3 maintain 维持
+4 patriotic 爱国的
+5 transcended 超越
+6 triumph over 击败；战胜
+7 written off 已报废的
+8 dedication 献身；奉献
+9 prestige 声望
+10 nominate 任命；指定
+11 misfortune 厄运；不幸
+12 uphold 支持；维护
+13 diligent 勤奋的
+14 appointed 约定的；指定的
+15 deviate 偏离
+16 biased 有偏见的
+17 assured 自信的
+18 for fear of 出于害怕
+19 wraps 包裹
+20 visible 看得见的
+21 hovered 徘徊
+22 in the background 在后台；在幕后
+23 flourish 繁荣、昌盛；茁壮成长
+24 take pride in 引以为豪
+25 get by 通过
+26 bold 大胆的
+27 scrawled 潦草的
+28 dreading 害怕
+29 grinning 笑着
+30 stern 严厉的；认真的
+31 intervene 介入
+32 recede 后退；消退
+33 bleak 凄凉的；暗淡的
+34 paralyze 麻痹
+35 symptom 症状
+36 deemed 认为
+37 pathetic 可怜的
+38 dismay 恐怖
+39 notorious 臭名昭著的
+40 mechanism 机制
+41 vulnerable 脆弱的
+42 bounced 弹跳
+43 appraise 评价
+44 dread 恐惧；害怕
+45 triumphantly 大获全胜地
+46 hold on 保持
+47 gnawing 令人痛苦的
+48 defy 反抗
+49 overwhelming 压倒性的
+50 indulge 放纵
+51 get a crush on
+52 nurture 培养
+53 self-restraint 自我克制
+54 compatibility 兼容
+55 curl up 变卷；蜷缩
+56 haste 匆忙
+57 exhilarated 兴奋的
+58 originate 起源
+59 brag 吹嘘
+60 affection 喜爱
+61 portray 描述
+62 rigorous 严格的
+63 afflict 受··的折磨
+64 ideology 意识形态
+65 stricken with 被··所震惊
+66 immigrant 移民
+67 conscientious 勤勉认真的
+68 captive 俘虏
+69 majesty 陛下
+70 compliment 赞扬
+71 deprive 剥夺
+72 jeopardize 危及
+73 noteworthy 明显的
+74 embark on 开始
+75 perpetual 一再反复的
+76 distinguish from 区别于
+77 in so far as 就目前而言
+78 grounded 建立于··之上
+79 break up 分手
+80 incidental 次要的
+81 surpass 胜过
+82 superiority 优势
+83 greed 贪得无厌
+84 virtuous 品德高尚的
+85 as to 至于；关于
+86 property 地产
+87 ethic 伦理
+88 liberality胸襟宽广
+89 impermanent 临时的
+90 attributed 归因于
+91 gauged 测量
+92 dedicate 献出
+93 commonplace 普遍的
+94 suffice 足以
+95 revenue 总收入
+96 simultaneous 同时发生的
+97 incentive 激励
+98 prone 有··倾向的
+99 innovations 创新
+100 fostered 促成；培养
+101 correlate with 相关与
+102 gossiping 流言蜚语
+103 coincided with 与··一致
+104 contended with 与··竞争
+105 conformed to 符合
+106 congratulate 祝贺
+107 melted down 融化
+108 proceed 继续
+109 look the part 看上去像那种人
+110 hot under the collar 困窘的；尴尬的；发怒的
+111 disciplines 训练；锻炼
+112 fake 假的
+113 take a grip on 抓住 
+114 clung to 紧紧抓住
+115 qualify 取得资格
+116 twisted my arm 扭我的胳膊
+117 ebb away 减弱；衰退
+118 strikingly 出众地
+119 under wraps 保密
+120 be in for a shock 可能会吃惊
+121 indignation 愤慨
+122 provocative 挑衅的
+123 dispense 分发
+124 cradle 摇篮
+125 disposal of waste 废物处理
+126 extinguish 扑灭
+127 preach 说教
+128 had authority over  拥有··的权力
+129 be conferred 授予
+130 curfew 宵禁
+131 obese 肥胖的
+132 gross 总收入
+133 baffled 感到困惑的
+134 decline 衰减
+135 milestone 重要事件
+136 filtered 过滤
+137 attributes 属性
+138 labels 标签
+139 accelerated 加速
+140 sponsoring 赞助
+141 equip with 使··配备、装备
+142 processed 加工过的；
+143 coincide with 与··相符、一致
+144 resumes 重新开始
+145 eccentricities 怪癖
+146 learning form 
+147 subtract 减去
+148 subject 话题
+149 pharmacy 药房
+150 synchronization 校准
+*/
+void init(){
+	string t;
+	while(getline(cin,t)){
+		if(t=="")break;
+		reference.push_back(Word());
+		int f=0;
+		for(int i=t.size();i;i--){
+			if(t[i]==' '){
+				if(f==0){
+					reference.back().mean=t.substr(i);
+					reference.back().wd=t.substr(t.find(" "),i-t.find(" "));
+					break;
+				}
+			}
+		}
+	}
+}
+int all=0,correct=0;
+void show(){
+	cout<<"all: "<<all<<"   correct: "<<correct<<"    ---";
+	if(all)cout<<100.0*correct/all;
+	else cout<<0;
+	cout<<"%\n";
+}
+void AC(){
+	cout<<"Accept\n";
+	all++;
+	correct++;
+}
+void WA(){
+	cout<<"Wrong Answer\n";
+	all++;
+}
+mt19937 Rad(time(0));
+void line(){
+	int s=20;
+	while(s--)cout<<"-";
+	cout<<"\n";
+}
+void solve(){
+	init();
+//	for(auto i:reference){
+//		cout<<i.wd<<" "<<i.mean<<"\n";
+//	}
+	while(1){
+		//49 50 51 52
+		line();
+		set<int>key;
+		int ans;
+		while(key.size()<4){
+			ans=(Rad()%((int)reference.size()));
+			key.insert(ans);
+		}
+		vector<int>Keys;
+		for(auto &i:key)Keys.push_back(i);
+		shuffle(Keys.begin(),Keys.end(),default_random_engine(time(0)));
+		cout<<reference[ans].wd<<"\n";
+		int tans=0;
+		for(int i=0;i<4;i++){
+			if(Keys[i]==ans)tans=i;
+			cout<<i+1<<" "<<reference[Keys[i]].mean<<"\n";
+		}
+		int t=getch();
+		if(t-49==tans)AC();
+		else WA();
+		line();
+		for(int i=0;i<4;i++){
+			if(Keys[i]==ans)tans=i;
+			cout<<i+1<<" "<<reference[Keys[i]].wd<<" "<<reference[Keys[i]].mean<<"\n";
+		}
+		line();
+		show();
+		line();
+		cout<<"\n\n";
+	}
+	
+	return;
+}
+
+signed main(){
+	int ____=1;
+//	cin>>____;
+	while(____--)solve();
+	return 0;
+}
